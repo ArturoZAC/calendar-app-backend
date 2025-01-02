2bae00c75c0471439bfe2543299aba0e5a0fb80b
4z4c <arturoyz2105@gmail.com>
2025-01-02 17:38:33 -0500

    last update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from "cors"
 import 'dotenv/config';
+import path from "path"
 import { dbConnection } from './database/config.js';
 import { authRouter } from './routes/auth.js'
 import { eventsRouter } from './routes/events.js';
@@ -23,6 +24,10 @@ app.use( express.json() );
 //* Rutas
 app.use('/api/auth', authRouter );
 app.use('/api/events', eventsRouter );
+
+app.use('*', (req,res) => {
+  res.sendFile( path.join(__dirname, 'public/index.html') )
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Estoy vivo en el port: ${process.env.PORT}`)
